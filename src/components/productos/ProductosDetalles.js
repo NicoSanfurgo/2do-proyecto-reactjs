33fb7d15d9b082a879a60851c619c88cd70f5a00
nicosanfurgo <nicosanmza@hotmail.com>
2022-12-16 21:07:15 -0300

    arreglos

--- src/components/productos/ProductosDetalles.js
+++ src/components/productos/ProductosDetalles.js
@@ -30,14 +30,12 @@ export const ProductosDetalles = () => {
     {
         <div className="detalles">
           <h2>{detalle.title}</h2>
+          <img src={detalle.image} />
           <p className="price">${detalle.price}</p>
           <button onClick={() => addCarrito(detalle.id)}>
             Añadir al carrito
           </button>
           
-          {
-            url ? <img src={images} alt={detalle.title}/> : <img src={detalle.image} alt={detalle.title}/>
-          }
           
           <div className="description">
           <p><b>description: </b> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum necessitatibus soluta alias porro, saepe facere expedita asperiores quos fugit inventore ex, itaque sapiente quae pariatur beatae optio repellat aperiam quia possimus mollitia repellendus? Illo natus quam eaque impedit omnis pariatur!</p>
