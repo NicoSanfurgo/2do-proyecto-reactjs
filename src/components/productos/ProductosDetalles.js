7cb0c0a522538a86e2ab1415a7e4be689c57fa65
nicosanfurgo <nicosanmza@hotmail.com>
2022-12-16 21:08:32 -0300

    arreglos

--- src/components/productos/ProductosDetalles.js
+++ src/components/productos/ProductosDetalles.js
@@ -38,9 +38,7 @@ export const ProductosDetalles = () => {
           
           
           <div className="description">
-          <p><b>description: </b> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum necessitatibus soluta alias porro, saepe facere expedita asperiores quos fugit inventore ex, itaque sapiente quae pariatur beatae optio repellat aperiam quia possimus mollitia repellendus? Illo natus quam eaque impedit omnis pariatur!</p>
-          <br/>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam vitae accusantium omnis, facere laudantium ipsa hic reprehenderit blanditiis quibusdam quos repellendus id illo reiciendis magni, aliquid beatae, consequatur sapiente! Sequi facere itaque,</p>
+          <p><b>description: </b> {detalle.detalle} </p>
           </div>
         </div>
     }
